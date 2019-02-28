7b00036df6b23cdda5a04db7980731c4b60ec182
胡小宁 <123794909@qq.com>
2019-02-28 21:02:31 +0800

    提交

--- src/pages/eat-home/eat-home.vue
+++ src/pages/eat-home/eat-home.vue
@@ -66,7 +66,7 @@ export default {
 		}
 	},
 	mounted () {
-		this.getPosition()
+		// this.getPosition()
 		this.getBaseCategoryList()
 		this.getBaseRestaurantList()
 		// this.getCateringRecommendList()
